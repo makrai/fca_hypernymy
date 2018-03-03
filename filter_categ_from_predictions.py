623f534d99754c1171a80e0cfd3bbcc7f1e273c1
makrai <makraimarton@gmail.com>
2018-03-04 00:11:44 +0100

    class

--- filter_categ_from_predictions.py
+++ filter_categ_from_predictions.py
@@ -2,8 +2,8 @@ import argparse
 import logging
 import os
 
-# from evaluate_dag import ThreeHundredSparsians
-# from evaluate_dag import get_args as parse_dummy_args
+from evaluate_dag import ThreeHundredSparsians
+from evaluate_dag import get_args as parse_dummy_args
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -25,68 +25,87 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
-    pred_dir, pred_basefn = os.path.split(args.predictions)
-    subtask = pred_basefn.split('_')[0]
-    assert ('dev' in args.predictions) != ('test' in args.predictions)
-    phase = 'trial' if 'dev' in args.predictions else 'test'
-    proj_dir = '/mnt/store/friend/proj/SemEval18-hypernym/'
+class CategoryFilter():
+    def __init__(self):
+        self.args = parse_args()
+        self.pred_dir, self.pred_basefn = os.path.split(self.args.predictions)
+        subtask = self.pred_basefn.split('_')[0]
+        assert ('dev' in self.args.predictions) != (
+            'test' in self.args.predictions)
+        phase = 'trial' if 'dev' in self.args.predictions else 'test'
+        proj_dir = '/mnt/store/friend/proj/SemEval18-hypernym/'
 
-    def input_fn(data_or_gold):
-        input_dir = os.path.join(
-            proj_dir, 'SemEval18-Task9/{}/{}'.format(phase, data_or_gold))
-        input_basefn = '{}.{}.{}.txt'.format(subtask, phase, data_or_gold)
-        return input_dir, input_basefn
-    query_dir, query_basefn = input_fn('data')
-    gold_dir, gold_basefn = input_fn('gold')
-    for categ in ['concept', 'entity']:
-        for all_dir in query_dir, gold_dir:
-            categ_dir = os.path.join(all_dir, categ)
+        def input_fn(data_or_gold):
+            input_dir = os.path.join(
+                proj_dir, 'SemEval18-Task9/{}/{}'.format(phase, data_or_gold))
+            input_basefn = '{}.{}.{}.txt'.format(subtask, phase, data_or_gold)
+            return input_dir, input_basefn
+        query_dir, query_basefn = input_fn('data')
+        gold_dir, gold_basefn = input_fn('gold')
+        for categ in ['concept', 'entity']:
+            for all_dir in query_dir, gold_dir:
+                categ_dir = os.path.join(all_dir, categ)
+                if not os.path.isdir(categ_dir):
+                    os.mkdir(categ_dir)
+            categ_dir = os.path.join(self.pred_dir, categ)
             if not os.path.isdir(categ_dir):
-                os.mkdir(categ_dir)
-        categ_dir = os.path.join(pred_dir, categ)
-        if not os.path.isdir(categ_dir):
-                os.mkdir(categ_dir)
-    query_path = os.path.join(query_dir, query_basefn)
-    gold_path = os.path.join(gold_dir, gold_basefn)
-    gold_concept_path = os.path.join(gold_dir, 'concept', gold_basefn)
-    gold_entity_path = os.path.join(gold_dir, 'entity', gold_basefn)
-    pred_concept_path = os.path.join(pred_dir, 'concept', pred_basefn)
-    pred_entity_path = os.path.join(pred_dir, 'entity', pred_basefn)
-    with open(query_path) as query_f, open(gold_path) as gold_f, \
-            open(args.predictions) as pred_f, \
-            open(gold_concept_path, mode='w') as gold_concept_f, \
-            open(gold_entity_path, mode='w') as gold_entity_f, \
-            open(pred_concept_path, mode='w') as pred_concept_f, \
-            open(pred_entity_path, mode='w') as pred_entity_f:
-        for line in query_f:
-            categ = line.strip().split('\t')[1]
-            gold_l = gold_f.readline()
-            pred_l = pred_f.readline()
-            if categ == 'Concept':
-                gold_concept_f.write(gold_l)
-                pred_concept_f.write(pred_l)
-            else:
-                gold_entity_f.write(gold_l)
-                pred_entity_f.write(pred_l)
-    """
-    dummy_args = parse_dummy_args()
-    dummy_args.file_struct = args.file_struct
-    sparsians = ThreeHundredSparsians(dummy_args)
-    sparsians.regularization = '1'  # TODO hack
-    for categ in ['concept', 'entity']:
-        metrics_categ = os.path.join(pred_dir, '../metrics/{}'.format(categ))
-        if not os.path.isdir(metrics_categ):
-            os.mkdir(metrics_categ)
-    metric_filen_patt = os.path.join(pred_dir, '../metrics/{}', pred_basefn)
-    logging.info('Writing metrics...')
-    sparsians.write_metrics(gold_concept_path, pred_concept_path,
-                            metric_filen_patt.format('concept'))
-    sparsians.write_metrics(gold_entity_path, pred_entity_path,
-                            metric_filen_patt.format('entity'))
-    """
+                    os.mkdir(categ_dir)
+        self.query_path = os.path.join(query_dir, query_basefn)
+        self.gold_path = os.path.join(gold_dir, gold_basefn)
+        self.gold_concept_path = os.path.join(gold_dir, 'concept', gold_basefn)
+        self.gold_entity_path = os.path.join(gold_dir, 'entity', gold_basefn)
+        self.pred_concept_path = os.path.join(self.pred_dir, 'concept',
+                                              self.pred_basefn)
+        self.pred_entity_path = os.path.join(self.pred_dir, 'entity',
+                                             self.pred_basefn)
+
+    def filter_by_categ(self):
+        logging.info('Filtering...')
+        with open(self.query_path) as query_f, \
+                open(self.gold_path) as gold_f, \
+                open(self.args.predictions) as pred_f, \
+                open(self.gold_concept_path, mode='w') as gold_concept_f, \
+                open(self.gold_entity_path, mode='w') as gold_entity_f, \
+                open(self.pred_concept_path, mode='w') as pred_concept_f, \
+                open(self.pred_entity_path, mode='w') as pred_entity_f:
+            for line in query_f:
+                categ = line.strip().split('\t')[1]
+                gold_l = gold_f.readline()
+                pred_l = pred_f.readline()
+                if categ == 'Concept':
+                    gold_concept_f.write(gold_l)
+                    pred_concept_f.write(pred_l)
+                else:
+                    gold_entity_f.write(gold_l)
+                    pred_entity_f.write(pred_l)
+
+    def write_metrics_wrapper(self):
+        if not os.path.isfile(self.gold_concept_path):  #TODO
+            self.filter_by_categ()
+        # TODO The wrapper around write_metrics is done in a hacky way,
+        # both argparsers run. In order for this code to work, a dummy
+        # '--predictions' argument should be added to the argparser in
+        # evaluate_dag
+        dummy_args = parse_dummy_args()
+        dummy_args.file_struct = self.args.file_struct
+        sparsians = ThreeHundredSparsians(dummy_args)
+        sparsians.regularization = 1
+        for categ in ['concept', 'entity']:
+            metrics_categ = os.path.join(self.pred_dir,
+                                         '../metrics/{}'.format(categ))
+            if not os.path.isdir(metrics_categ):
+                os.mkdir(metrics_categ)
+        metric_filen_patt = os.path.join(self.pred_dir, '../metrics/{}',
+                                         self.pred_basefn)
+        logging.info('Writing metrics...')
+        sparsians.write_metrics(self.gold_path, self.args.predictions,
+                                metric_filen_patt.format(''))
+        sparsians.write_metrics(self.gold_concept_path, self.pred_concept_path,
+                                metric_filen_patt.format('concept'))
+        sparsians.write_metrics(self.gold_entity_path, self.pred_entity_path,
+                                metric_filen_patt.format('entity'))
 
 
 if __name__ == '__main__':
-    main()
+    cfilter = CategoryFilter() 
+    cfilter.write_metrics_wrapper()
